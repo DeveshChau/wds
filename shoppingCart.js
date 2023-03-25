2189f8459472db91e824853c0985ee5a5f0b8d76
DeveshChau <deveshachaudhari@gmail.com>
2023-03-25 11:16:19 +0900

    handle multiple of same item in the cart

--- shoppingCart.js
+++ shoppingCart.js
@@ -7,6 +7,7 @@ let shoppingCart = [];
 const cartItemTemplate = document.querySelector('#cart-item-template');
 const cartItemContainer = document.querySelector('[data-cart-items]');
 const IMAGE_URL = 'https://dummyimage.com/210x130';
+const cartQuantity = document.querySelector('[data-cart-quantity]');
 
 export function setUpShoppingcart() {
     cartButton.addEventListener('click', () => {
@@ -15,12 +16,19 @@ export function setUpShoppingcart() {
 }
 
 export function addToCart(id) {
-    shoppingCart.push({id: id, quantity: 1});
+    const existingItem = shoppingCart.find(entry => entry.id === id);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        shoppingCart.push({id: id, quantity: 1});        
+    }
     renderCart();
 }
 
 function renderCart() {
     cartItemContainer.innerHTML = '';
+    cartQuantity.innerHTML = shoppingCart.length;
+    
     shoppingCart.forEach(entry => {
         const item = items.find(i => entry.id === i.id)
         const cartItem = cartItemTemplate.content.cloneNode(true)
@@ -34,8 +42,10 @@ function renderCart() {
         const image = cartItem.querySelector("[data-image]")
         image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
 
-        const quantity = cartItem.querySelector("[data-quantity]")
-        quantity.innerText = `x${entry.quantity}`
+        if (entry.quantity > 1) {
+            const quantity = cartItem.querySelector("[data-quantity]")
+            quantity.innerText = `x${entry.quantity}`            
+        }
 
         const price = cartItem.querySelector("[data-price]")
         price.innerText = formatCurrency((item.priceCents * entry.quantity) / 100)
